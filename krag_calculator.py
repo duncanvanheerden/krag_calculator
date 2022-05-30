8e94b2656589eb17170ee6b9616453618ccdc796
Duncan Van Heerden <duncanvanheerden29@gmail.com>
2022-05-30 18:52:00 +0200

    clean

--- krag_calculator.py
+++ krag_calculator.py
@@ -1,8 +1,7 @@
 while True:
 
     user_input = input("Enter the amount you wish to purchase: R ")
-
-    # input validation  
+ 
     if user_input.isnumeric():
         price_in_rands = float(user_input)
         vat = price_in_rands / 1.15
@@ -13,9 +12,7 @@ while True:
 
         if price_in_rands <= 0:
             print("Error, amount cannot be < or = 0.0")
-
-     
+    
     elif user_input.isalpha or user_input.isalnum:
         print("invalid")  
-        continue
-    # continuous input    
+        continue   
