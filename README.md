d073da026ec0f15995a75c4059f0eae0b6b1a15e
Duncan Van Heerden <duncanvanheerden29@gmail.com>
2022-05-30 17:24:07 +0200

    program works as intended
    / input validation
    / errors handled
    / continuous input

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # krag_calculator
 1 - this short program converts the user_input(price in Rands) to units of electricity(kwh).
-2 - for the most part it works but.
-3 - I would like the program to receive continuous user_input and validate user_input(numeric value or string).
+2 - for the most part it works.
+3 - i would like to make it a webapp.
--- krag_calculator.py
+++ krag_calculator.py
@@ -1,14 +1,18 @@
-def krag_calc():
+while True:
 
-    price_in_rands = float(user_input)
-    vat = price_in_rands / 1.15
+    user_input = input("Enter the amount you wish to purchase: R ")
 
-    if price_in_rands > 0: 
-        price_in_rands = vat * 0.43 
-        print("Units:", round(price_in_rands, 2))
-    if price_in_rands <= 0:
-        print("Error, amount cannot be < or = 0.0")
-    return price_in_rands
+    if user_input.isnumeric():
+        price_in_rands = float(user_input)
+        vat = price_in_rands / 1.15
 
-user_input = input("Enter the amount you wish to purchase: R ")
-krag_calc()    
+        if price_in_rands > 0: 
+            price_in_rands = vat * 0.43 
+            print("Units:", round(price_in_rands, 2))
+
+        if price_in_rands <= 0:
+            print("Error, amount cannot be < or = 0.0")
+
+    elif user_input.isalpha or user_input.isalnum:
+        print("invalid")
+        continue    
